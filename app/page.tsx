9a5a340718b5d2bf60d2730831cdbef0f6d8008a
Devam <44005330+Devam338@users.noreply.github.com>
2026-04-26 16:21:11 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+ 
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useState, useEffect, useCallback } from "react";
 import { ContactCard } from "@/components/ContactCard";
